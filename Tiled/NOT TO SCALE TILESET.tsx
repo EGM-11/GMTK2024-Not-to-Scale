7d23e7be6889df4b93dec24160019a8dbdd16d39
PAIKEA <59355185+EGM-11@users.noreply.github.com>
2024-08-19 15:35:31 -0700

    Tileset stuff

---
+++ Tiled/NOT TO SCALE TILESET.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="NOT TO SCALE TILESET" tilewidth="32" tileheight="32" tilecount="513" columns="27">
+ <image source="Not to Scale Tileset.png" width="864" height="608"/>
+</tileset>
